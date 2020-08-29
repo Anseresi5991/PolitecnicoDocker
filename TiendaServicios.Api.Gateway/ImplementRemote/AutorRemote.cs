9b0f5b5261910090be050140fa807d99c338b57b
Andres Sierra <andres_sebastian2009@hotmail.com>
2020-08-29 00:56:20 -0500

    Mensajeria RabbitMQ

---
+++ TiendaServicios.Api.Gateway/ImplementRemote/AutorRemote.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Gateway.InterfaceRemote;
+using TiendaServicios.Api.Gateway.RemoteModel;
+
+namespace TiendaServicios.Api.Gateway.ImplementRemote
+{
+    public class AutorRemote : IAutorRemote
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly ILogger<AutorRemote> _logger;
+
+        public AutorRemote(IHttpClientFactory httpClient, ILogger<AutorRemote> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+        public async Task<(bool resultado, AutorModeloRemote autor, string ErrorMessage)> GetAutor(Guid AutorId)
+        {
+            try
+            {
+                HttpClient cliente = _httpClient.CreateClient("AutorService");
+                HttpResponseMessage response = await cliente.GetAsync($"/Autor/Autor?id={AutorId}");
+                if (response.IsSuccessStatusCode)
+                {
+                    string contenido = await response.Content.ReadAsStringAsync();
+                    JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                    AutorModeloRemote resultado = JsonSerializer.Deserialize<AutorModeloRemote>(contenido,options);
+                    return (true, resultado, null);
+                }
+                return (false, null, response.ReasonPhrase);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+                return (false, null, e.Message);
+
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TiendaServicios.Api.Gateway/MessageHandler/LibroHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Gateway.InterfaceRemote;
+using TiendaServicios.Api.Gateway.LibroRemote;
+using TiendaServicios.Api.Gateway.RemoteModel;
+
+namespace TiendaServicios.Api.Gateway.MessageHandler
+{
+    public class LibroHandler:DelegatingHandler
+    {
+        private readonly ILogger<LibroHandler> _logger;
+        private readonly IAutorRemote _autorRemote;
+        public LibroHandler(ILogger<LibroHandler> logger,IAutorRemote autorRemote)
+        {
+            _autorRemote = autorRemote;
+            _logger = logger;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Stopwatch tiempo = Stopwatch.StartNew();
+            _logger.LogInformation("Inicia el request");
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+            if (response.IsSuccessStatusCode)
+            {
+                string contenido = await response.Content.ReadAsStringAsync();
+                JsonSerializerOptions options = new JsonSerializerOptions{PropertyNameCaseInsensitive=true};
+                LibroModeloRemote resultado = JsonSerializer.Deserialize<LibroModeloRemote>(contenido, options);
+                var responseAutor= await _autorRemote.GetAutor(resultado.AutorLibro??Guid.Empty);
+                if (responseAutor.resultado)
+                {
+                    AutorModeloRemote objetoAutor = responseAutor.autor;
+                    resultado.AutorData = objetoAutor;
+                    string resultadoStr = JsonSerializer.Serialize(resultado);
+                    response.Content = new StringContent(resultadoStr, System.Text.Encoding.UTF8, "application/json");
+                }
+
+            }
+            _logger.LogInformation($"Estre proceso se hizo en {tiempo.ElapsedMilliseconds}ms");
+            return response;
+        }
+    }
+}
